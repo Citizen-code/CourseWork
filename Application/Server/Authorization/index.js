38e84e3234eccc0898d5f5159b91bca1c4b6c808
Citizen <kostya20041019@yandex.ru>
2023-11-07 00:57:34 +0300

    Тестирование cors

--- Application/Server/Authorization/index.js
+++ Application/Server/Authorization/index.js
@@ -9,9 +9,7 @@ let PORT = process.env.PORT || 5000
 
 let app = express()
 
-app.use(cors({
-    origin: '*'
-}));
+app.use(cors());
 app.use(express.json())
 app.use(cookieParser())
 app.use("/auth", router)
