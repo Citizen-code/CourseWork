d6fe6a26cbe6bd9359cdffdd21a2327c40f75622
Citizen <kostya20041019@yandex.ru>
2023-11-24 11:41:35 +0300

    Добавлена галерея

--- Application/Client/car-service/src/components/home/gallery.tsx
+++ Application/Client/car-service/src/components/home/gallery.tsx
@@ -1,5 +1,5 @@
 import style from '@/components/style/home/gallery.module.css'
-import items from '@/data/service'
+import items from '@/data/gallery'
 
 export default function Gallery(){
     return(
@@ -7,9 +7,9 @@ export default function Gallery(){
             <div className='container h-100'>
                 <h1 className={style['h1']}>Галерея</h1>
                 <div className='row align-items-top'>
-                    {items.map(item=> 
-                        <div key={item.id} className={`col-6 col-md-4 ${style['element']}`}>
-                            <img src={item.image} height={250} width={'100%'} alt="Фото услуги"/>
+                    {items.map((item,index)=> 
+                        <div key={index} className={`col-6 col-md-4 ${style['element']}`}>
+                            <img src={item} height={250} width={'100%'} alt="Фото услуги"/>
                         </div>
                     )}
                 </div>
---
+++ Application/Client/car-service/src/data/gallery.ts
@@ -0,0 +1,12 @@
+const GalleryItem = [
+    '/428a6eb7-7e5f-480e-b7af-b35b8fb3e4d7.jpg',
+    '/53f939e1-3493-4ca5-9ea9-a82754d2a5ae.jpg',
+    '/ad50ca32-3050-40c5-8eee-57472daffa0f.jpg',
+    '/3adf1807-59ba-4f27-ae53-c8804b72f083.jpg',
+    '/ae63cb8e-4c63-480b-a0a7-70cbb1eef80b.webp',
+    '/87f33c0d-a753-42dd-912b-fef15b2867b8.jpg',
+    '/8f860d67-f04a-41a7-8a99-9418c51cca3f.jpg',
+    '/117b9bf8-f414-4198-8fb2-b519b0e5a373.jpg'
+]
+
+export default GalleryItem
